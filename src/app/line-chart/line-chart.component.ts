fa3ae8b54b3351bf180523cbaa9aac07475cd12d
unknown <AbdeChk>
2023-05-11 09:01:48 +0100

    front_update

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// axios : Promise based HTTP client for the browser and node.js
 import axios from 'axios';
 import Chart from 'chart.js/auto';
 
@@ -12,14 +11,26 @@ const API_URL = 'https://api.coingecko.com/api/v3';
 })
 export class LineChartComponent {
   chart: any;
-  
+
 
   async createChart() {
     const endDate = new Date();
     const startDate = new Date();
+    const emptyValue = Array(90).fill(null);
+    let n_dates: string[] = [];
+    let n_predictions: number[] = [];
+
+
     startDate.setMonth(endDate.getMonth() - 1);
 
-    console.log("the end;" + endDate)
+    const input =
+    {
+      "day": 12,
+      "month": 5,
+      "yesterday_price": 27639,
+      "forecast_days": 10
+    }
+
 
     const response = await axios.get(`${API_URL}/coins/bitcoin/market_chart`, {
       params: {
@@ -28,20 +39,37 @@ export class LineChartComponent {
         interval: 'daily'
       },
     });
-    // example response =
-    // https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=hourly
+    await axios.post('http://127.0.0.1:5000/predict', input )
+      .then(response => {
+        const data = response.data;
+        n_dates = data.predictions.map((prediction: { date: string; prediction: number }) => prediction.date);
+        n_predictions = data.predictions.map((prediction: { date: string; prediction: number }) => prediction.prediction);
+      
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
 
 
+
+    const newdata = emptyValue.concat(n_predictions);
     const prices = response.data.prices;
     const timeStamps = prices.map((price: any) => new Date(price[0]));
 
     // CONVERT timeStamps FROM '' TO 'Apr 26, 2023, 4:01:09 AM'
     let time_stamp = [];
-    for(var date of timeStamps){
+    for (var date of timeStamps) {
       time_stamp.push(this.convert_date(date));
+      //console.log(typeof this.convert_date(date))
     }
-    console.log(time_stamp)
+    //(time_stamp:any)=>time_stamp.pop()
+    time_stamp=time_stamp.slice(0,-1)
 
+    time_stamp = time_stamp.concat(n_dates)
+    
+    let  con_data=prices.map((price: any) => price[1])
+    con_data=con_data.slice(0,-1)
+    console.log(con_data)
     // DEFINE THE LINE CHART
     this.chart = new Chart("MyChart", {
       type: 'line',
@@ -51,19 +79,20 @@ export class LineChartComponent {
           {
             // real price plot
             label: "Bitcoin Price (USD)",
-            data: prices.map((price: any) => price[1]),
+            data: con_data,
             backgroundColor: 'blue'
           },
           {
             // predictive price plot
             label: "Predictive Bitcoin Price (USD)",
-            data: prices.map((price: any) => price[1]),
+            // data: prices.map((price: any) => price[1]),
+            data: newdata,
             backgroundColor: 'red'
           }
         ],
-        
+
       },
-    
+
       options: {
         aspectRatio: 2
       }
@@ -74,17 +103,17 @@ export class LineChartComponent {
     this.createChart();
   }
 
-  convert_date(date : Date){
+  convert_date(date: Date) {
     // take date = "Wed Apr 26 2023 04:01:09 GMT+0200 (heure d’été d’Europe centrale)"
     // return Output: Apr 26, 2023, 4:01:09 AM
-      const formattedDate = date.toLocaleString("en-US", { 
-      year: 'numeric', 
-      month: 'short', 
-      day: '2-digit', 
-      hour: '2-digit', 
-      minute: '2-digit', 
-      second: '2-digit',
-      timeZone: 'Europe/Paris'
+    const formattedDate = date.toLocaleString("en-US", {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit'
+      //hour: '2-digit', 
+      //minute: '2-digit', 
+      //second: '2-digit',
+      //timeZone: 'Europe/Paris'
     });
     return formattedDate;
   }
